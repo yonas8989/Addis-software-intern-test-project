8db79acb163f1451e8ee70bc0e86478b2bd23fab
yonas_fikadie <yonasfikadie8989@gmail.com>
2024-09-28 18:59:39 -0700

    fixing issues for installing tailwind css

--- song_shelf/src/App.jsx
+++ song_shelf/src/App.jsx
@@ -3,6 +3,7 @@ import React from "react";
 function App() {
   return (
     <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
       <button class="bg-cyan-500 hover:bg-cyan-600 ...">Subscribe</button>
     </div>
   );
