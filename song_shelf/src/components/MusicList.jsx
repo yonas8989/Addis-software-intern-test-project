ca64a83e1085311f53d772333a334c4731c104b4
yonas_fikadie <yonasfikadie8989@gmail.com>
2024-10-08 21:43:18 +0200

    add skeleton to the music card component

--- song_shelf/src/components/MusicList.jsx
+++ song_shelf/src/components/MusicList.jsx
@@ -1,23 +1,28 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from 'react-redux';
 import MusicCard from "./MusicCard";
-import FilterComponent from "../components/filter components/FilterComponent";
+import FilterComponent from "./filter components/FilterComponent";
+import Footer from "./Footer";
+import SongActions from "./SongActions";
 import PromotionComponent from "./PromotionComponent";
-import useSongActions from "../hooks/userSongActions"; // Correct import
+import useSongActions from '../hooks/userSongActions';
+import SkeletonCard from "./SkeletonCard"; // Import SkeletonCard component
 
 const MusicList = () => {
-  const { fetchSongs, deleteSong, editSong } = useSongActions(); // Get song actions
+  const { fetchSongs } = useSongActions();
+  const dispatch = useDispatch();
 
-  const { songs, loading, error } = useSelector((state) => state.songs); // Get songs from Redux
+  // Select songs from Redux store
+  const { songs, loading, error } = useSelector((state) => state.songs);
 
   const [filteredSongs, setFilteredSongs] = useState(songs);
 
   useEffect(() => {
-    fetchSongs(); // Fetch songs on component mount
+    fetchSongs(); // Fetch songs when component mounts
   }, [fetchSongs]);
 
   useEffect(() => {
-    setFilteredSongs(songs); // Update filtered songs when the song list changes
+    setFilteredSongs(songs); // Update filtered songs when songs are fetched
   }, [songs]);
 
   const filterSongs = (genre, artist, album) => {
@@ -33,41 +38,32 @@ const MusicList = () => {
 
   return (
     <div className="music-list-container">
-    {loading && <p className="text-slate-500">Loading...</p>}
-    {error && <p>Error: {error}</p>}
+      {error && <p>Error: {error}</p>}
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         <div className="bg-blue-50 flex flex-col justify-between h-full p-4">
           <FilterComponent onFilterChange={filterSongs} />
-          <PromotionComponent
-            title={"mar eske tuafi "}
-            artist={"Tedy Afro"}
-            albumCover={
-              "https://images.pexels.com/photos/28304389/pexels-photo-28304389/free-photo-of-a-person-holding-a-flower-in-front-of-the-ocean.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
-            }
-          />
-          <PromotionComponent
-            title={"mar eske tuafi "}
-            artist={"Tedy Afro"}
-            albumCover={
-              "https://images.pexels.com/photos/28304389/pexels-photo-28304389/free-photo-of-a-person-holding-a-flower-in-front-of-the-ocean.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
-            }
-          />
+          <PromotionComponent title={"mar eske tuafi "} artist={'tedy afro'} albumCover={"https://images.pexels.com/photos/28304389/pexels-photo-28304389/free-photo-of-a-person-holding-a-flower-in-front-of-the-ocean.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"} />
+          <PromotionComponent title={"mar eske tuafi "} artist={'tedy afro'} albumCover={"https://images.pexels.com/photos/28304389/pexels-photo-28304389/free-photo-of-a-person-holding-a-flower-in-front-of-the-ocean.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"} />
+          <div className="mt-4">
+            <SongActions />
+          </div>
         </div>
         <div className="col-span-1 md:col-span-2 lg:col-span-3 bg-gray-900">
           <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5 gap-4">
-            {filteredSongs.map((song) => (
-              <MusicCard
-                key={song.id}
-                id={song.id}
-                title={song.title}
-                genre={song.genre}
-                album={song.album}
-                artistImage={song.artistImage}
-                onEditSong={editSong} // Pass the edit function
-                onDeleteSong={deleteSong} // Pass the delete function
-              />
-            ))}
-            
+            {!loading
+              ? Array.from({ length: 50 }).map((_, index) => (
+                  <SkeletonCard key={index} /> // Render 10 skeleton cards
+                ))
+              : filteredSongs.map((song) => (
+                  <MusicCard
+                    key={song.id}
+                    id={song.id}  // Pass the song id here
+                    title={song.title}
+                    genre={song.genre}
+                    album={song.album}
+                    artistImage={song.artistImage}
+                  />
+                ))}
           </div>
         </div>
       </div>
