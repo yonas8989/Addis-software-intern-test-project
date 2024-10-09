473e7dd9983e8dcddbbe678e71aecab39c26493f
yonas_fikadie <yonasfikadie8989@gmail.com>
2024-10-09 02:27:54 +0200

    remove un used imports

--- song_shelf/src/components/MusicList.jsx
+++ song_shelf/src/components/MusicList.jsx
@@ -1,15 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from "react-redux";
 import MusicCard from "./MusicCard";
 import FilterComponent from "./filter components/FilterComponent";
-import SongActions from "./SongActions";
 import PromotionComponent from "./PromotionComponent";
-import useSongActions from '../hooks/userSongActions';
+import useSongActions from "../hooks/userSongActions";
 import SkeletonCard from "./SkeletonCard"; // Import SkeletonCard component
 
 const MusicList = () => {
   const { fetchSongs } = useSongActions();
-  const dispatch = useDispatch();
 
   // Select songs from Redux store
   const { songs, loading, error } = useSelector((state) => state.songs);
@@ -17,8 +15,8 @@ const MusicList = () => {
   const [filteredSongs, setFilteredSongs] = useState(songs);
 
   useEffect(() => {
-    fetchSongs(); // Fetch songs when component mounts
-  }, [fetchSongs]);
+      fetchSongs(); // Fetch songs when component mounts
+    }, [fetchSongs]);
 
   useEffect(() => {
     setFilteredSongs(songs); // Update filtered songs when songs are fetched
@@ -41,11 +39,17 @@ const MusicList = () => {
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
         <div className="bg-blue-50 flex flex-col justify-between h-full p-4">
           <FilterComponent onFilterChange={filterSongs} />
-          <PromotionComponent title={"mar eske tuafi "} artist={'tedy afro'} albumCover={"https://images.pexels.com/photos/28304389/pexels-photo-28304389/free-photo-of-a-person-holding-a-flower-in-front-of-the-ocean.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"} />
+          <PromotionComponent
+            title={"mar eske tuafi "}
+            artist={"tedy afro"}
+            albumCover={
+              "https://images.pexels.com/photos/28304389/pexels-photo-28304389/free-photo-of-a-person-holding-a-flower-in-front-of-the-ocean.jpeg?auto=compress&cs=tinysrgb&w=300&lazy=load"
+            }
+          />
         </div>
         <div className="col-span-1 md:col-span-2 lg:col-span-3 bg-gray-900">
           <div className="grid grid-cols-1 md:grid-cols-4 lg:grid-cols-5 gap-4">
-            {!loading
+            {loading
               ? Array.from({ length: 50 }).map((_, index) => (
                   <SkeletonCard key={index} /> // Render 10 skeleton cards
                 ))
